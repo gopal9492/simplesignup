3acd189259eba9b91f26932e02d56efa16313be8
GopalSetty <gopal949222@email.com>
2021-07-07 22:23:59 +0530

    store password with encrypt

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const app = express();
-const route=require('./controller/router')
+//const route=require('./controller/router')
 app.use(express.json())
 require('./model/dbconfig')
+require('dotenv').config();
 
-app.use('/api/user',route);
+app.use('/api/user',require('./controller/router'));
 const port = process.env.PORT || 5000
 app.listen(port, () => {
     console.log(`Server Up in Port ${port}`);
--- controller/router.js
+++ controller/router.js
@@ -2,46 +2,45 @@ const express=require('express');
 const route=express.Router();
 const user=require('../model/signup');
 const {addUserValidation,userValidation}=require('../validations/uservalidation');
+const bcrypt = require('bcryptjs');
+//const CryptoJS=require('crypto-js')
+
+
 route.post('/signup',addUserValidation,async (req,res)=>{
     const {firstname,lastname,email,password}=req.body;
-    // if((firstname==null) || (lastname==null) ||( email ==null) || (password == null)){
-    //     res.send("The data is not allowed null ")
-    //     }
-    // else if((firstname.length===0)||(lastname.length===0) ||( email.length===0) || (password.length===0)){
-    //     res.send("The email length is not allowed zero")
-    // }  
+
     const deviceid=req.body.deviceid || " ";
     const appid=req.body.appid || " "
- 
-        await user.findOne({ email:email }).then(user => {
+    
+    const hashpsw=await bcrypt.hash(password,12);
+        await  user.findOne({ email:email}).then(user => {
             if (user){
                 res.send("the user is already exist")
                 return res.status(400).json(errors);
             }
         })
-        const newuser=await new user({firstname, lastname,email,password, deviceid,appid} )
-        await newuser.save();
+        const newuser= await new user({firstname, lastname,email,password:hashpsw, deviceid,appid} )
+       await newuser.save();
         return res.send("profile is added..")
 })
-route.post('/signin',userValidation,(req,res)=>{
+route.post('/signin',userValidation,async (req,res)=>{
         const {email,password}=req.body;
-    //    if(( email ==null) || (password == null)){
-    //         res.send("The data is not allowed null ")
-    //         }
-    //     else if(( email.length===0) || (password.length===0)){
-    //         res.send("The email length is not allowed zero")
-    //     }  
-         user.findOne({ email:req.body.email }).then(user => {
+       
+          user.findOne({ email:email }).then(user => {
+            const isMatch=  bcrypt.compare(password,user.password);
             if (!user){
-                res.send("the email id is wrong")
+                return res.send("the email id is wrong")
             }
-            else if(password == user.password){
+            
+          else if(!isMatch){
+                return res.send("the password is wrong")
+             }
+            else {
                 res.send(user);
             }
-            else{
-                res.send("password is not matched")
-            }
-        })
+            
+       })
+ 
        
 })
 module.exports=route;
