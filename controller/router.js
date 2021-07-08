11958453c88ff9f04234e9b25ad18cf79de4562b
GopalSetty <gopal949222@email.com>
2021-07-08 12:20:32 +0530

    store email with encryption format

--- controller/router.js
+++ controller/router.js
@@ -3,30 +3,29 @@ const route=express.Router();
 const user=require('../model/signup');
 const {addUserValidation,userValidation}=require('../validations/uservalidation');
 const bcrypt = require('bcryptjs');
-//const CryptoJS=require('crypto-js')
-
+const salt = bcrypt.genSaltSync(10);
 
 route.post('/signup',addUserValidation,async (req,res)=>{
     const {firstname,lastname,email,password}=req.body;
 
     const deviceid=req.body.deviceid || " ";
     const appid=req.body.appid || " "
-    
-    const hashpsw=await bcrypt.hash(password,12);
+    const hashemail=await bcrypt.hashSync(email,salt);
+    const hashpsw=await  bcrypt.hashSync(password, salt);
         await  user.findOne({ email:email}).then(user => {
             if (user){
                 res.send("the user is already exist")
                 return res.status(400).json(errors);
             }
         })
-        const newuser= await new user({firstname, lastname,email,password:hashpsw, deviceid,appid} )
+        const newuser= await new user({firstname, lastname,email:hashemail,password:hashpsw, deviceid,appid} )
        await newuser.save();
         return res.send("profile is added..")
 })
 route.post('/signin',userValidation,async (req,res)=>{
         const {email,password}=req.body;
-       
-          user.findOne({ email:email }).then(user => {
+        const hashemail=await bcrypt.hashSync(email,salt);
+          user.findOne({ email:hashemail}).then(user => {
             const isMatch=  bcrypt.compare(password,user.password);
             if (!user){
                 return res.send("the email id is wrong")
